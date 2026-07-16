ea95870bf49dcc43b857aadafcbce4b726c91f2d
cornpiess <zhuhong19950126@gmail.com>
2026-07-16 22:55:03 +0800

    chore: update bat file to include Node.js PATH
    - Added set PATH for Node.js
    - Removed --no-install flag from npx

--- 启动小鹦鹉.bat
+++ 启动小鹦鹉.bat
@@ -2,9 +2,10 @@
 chcp 65001 >nul
 title 小鹦鹉英语
 cd /d "%~dp0"
+set PATH=C:\Program Files\nodejs;%PATH%
 echo 正在启动小鹦鹉英语学习应用...
 echo 启动后请访问: http://localhost:8080
 echo 按 Ctrl+C 可停止服务
 echo.
-npx --no-install vite --host
+npx vite --host
 pause
